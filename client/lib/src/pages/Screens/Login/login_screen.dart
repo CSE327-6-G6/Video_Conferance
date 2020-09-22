b9400c6004c1ec0e2277821fb284bf6e8bc17a0d
JarinSayma <zarinsayma13@gmail.com>
2020-09-22 18:31:45 +0600

    Signup and Login UI

---
+++ client/lib/src/pages/Screens/Login/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:vChat_v1/src/pages/Screens/Login/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
