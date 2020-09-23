ee5a1541927f04291e9bd9efbe01c3642996f612
SOumic-Shekhar <soumic.aoyon@northsouth.edu>
2020-09-23 12:20:56 +0600

    lobby added

---
+++ client/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './src/pages/default.dart';
+// import 'package:vChat_v1/src/pages/Screens/Welcome/welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primaryColor: Colors.white,
+
+      ),
+      // home: WelcomeScreen(),
+      home : MyHomePage(),
+    );
+  }
+}
+
---
+++ client/lib/src/pages/default.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:vChat_v1/src/pages/bottom_navbar.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, initialIndex: 0, length: 2);
+  }
+
+    @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration.collapsed(
+            hintText: 'Search',
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.filter_list,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.purple,
+          labelColor: Colors.purple,
+          unselectedLabelColor: Theme.of(context).textTheme.caption.color,
+          isScrollable: false,
+          tabs: <Widget>[
+            Tab(
+              text: "Message",
+            ),
+            Tab(
+              text: "Groups",
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          // sets the background color of the `BottomNavigationBar`
+          canvasColor: Colors.white,
+          // sets the active color of the `BottomNavigationBar` if `Brightness` is light
+          primaryColor: Colors.purple,
+          textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle(color: Colors.black),
+              ),
+        ),
+        child: BottomNavBar(),
+      ),
+    );
+  }
+}
